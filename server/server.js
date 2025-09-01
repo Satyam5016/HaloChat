e3da5cdc3241bf4e92f9410727339ced33641890
Satyam5016 <satyamyadav4848@email.com>
2025-09-01 11:23:48 +0530

    Fix case sensitivity for message model

--- server/server.js
+++ server/server.js
@@ -48,5 +48,9 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => console.log("Server running on port:" + PORT));
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 8000;
+    server.listen(PORT, () => console.log("Server running on port:" + PORT));
+}
+
+export default server;
