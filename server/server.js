e65604bfd80d24e02c006906ba65f66d37adb7c4
Satyam5016 <satyamyadav4848@email.com>
2025-09-07 18:32:31 +0530

    comt

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ await connectDB();
 
 // Start server locally (for dev)
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 8000;
   server.listen(PORT, () => console.log("Server running on port:" + PORT));
 }
 
